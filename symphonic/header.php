1e3d7a5def00f00540073a9389327adf14c0bf79
Taylor <taylor.buswell@my.jcu.edu.au>
2023-05-06 16:51:08 +1000

    add header sub menu for testing

--- symphonic/header.php
+++ symphonic/header.php
@@ -13,47 +13,76 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'symphonic' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'symphonic' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$symphonic_description = get_bloginfo( 'description', 'display' );
-			if ( $symphonic_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $symphonic_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site-header">
+        <div class="site-branding">
+            <?php
+            the_custom_logo();
+            if ( is_front_page() && is_home() ) :
+                ?>
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <?php
+            else :
+                ?>
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+            <?php
+            endif;
+            $symphonic_description = get_bloginfo( 'description', 'display' );
+            if ( $symphonic_description || is_customize_preview() ) :
+                ?>
+                <p class="site-description"><?php echo $symphonic_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+            <?php endif; ?>
+        </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'symphonic' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        <nav id="site-navigation" class="main-navigation">
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'symphonic' ); ?></button>
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'menu-1',
+                    'menu_id'        => 'primary-menu',
+                )
+            );
+            ?>
+            <ul class="sub-menu">
+                <li><a href="#">Links</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav><!-- #site-navigation -->
+
+
+        <!-- Add mobile navigation menu -->
+        <div class="mobile-navigation">
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'menu-1',
+                    'menu_id'        => 'mobile-menu',
+                    'fallback_cb'    => 'my_fallback_menu',
+                )
+            );
+            ?>
+        </div>
+
+    </header><!-- #masthead -->
+
+    <!-- Add JavaScript to toggle mobile navigation menu -->
+    <script>
+        const menuToggle = document.querySelector('.menu-toggle');
+        const mobileNav = document.querySelector('.mobile-navigation');
+
+        menuToggle.addEventListener('click', () => {
+            mobileNav.classList.toggle('open');
+        });
+    </script>
