aa47a995fb8b5eced9aeba5747e4cf1fd78dc37f
Jack-Walsh <jack.walsh@my.jcu.edu.au>
2023-05-14 11:55:56 +1000

    removed unnecessary posts metadata removed page titles add themed css to paragraph and headers

--- symphonic/single.php
+++ symphonic/single.php
@@ -10,30 +10,45 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+    <main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+        <span class="posted-on"><?php esc_html_e('Posted on', 'your-theme'); ?><?php echo get_the_date(); ?></span>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+        <span class="byline">
+          <?php esc_html_e('By', 'your-theme'); ?>
+          <span class="author vcard">
+            <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>">
+              <?php echo get_the_author(); ?>
+            </a>
+          </span>
+        </span>
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'symphonic' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'symphonic' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+        <span class="posted-in"><?php esc_html_e( 'Posted in', 'your-theme' ); ?> <?php the_category(', '); ?></span>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
 
-		endwhile; // End of the loop.
-		?>
 
-	</main><!-- #main -->
+        <?php
+        while (have_posts()) :
+            the_post();
+
+            get_template_part('template-parts/content', get_post_type());
+
+            the_post_navigation(
+                array(
+                    'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'symphonic') . '</span> <span class="nav-title">%title</span>',
+                    'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'symphonic') . '</span> <span class="nav-title">%title</span>',
+                )
+            );
+
+            // If comments are open or we have at least one comment, load up the comment template.
+            if (comments_open() || get_comments_number()) :
+                comments_template();
+            endif;
+
+        endwhile; // End of the loop.
+        ?>
+
+    </main><!-- #main -->
 
 <?php
 get_sidebar();
