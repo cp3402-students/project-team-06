a38e855b2bc975d9218b38cfdca0bcdbbc76c5ac
Jack-Walsh <jack.walsh@my.jcu.edu.au>
2023-05-14 16:06:06 +1000

    add magazine posts page design (custom css) restructured css file

--- symphonic/footer.php
+++ symphonic/footer.php
@@ -11,36 +11,36 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		
-			
-<html>
+<footer id="colophon" class="site-footer">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Untitled</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
 
-<body>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Untitled</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+<!--        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    </head>
+
+    <body>
     <div class="footer-basic">
         <footer>
             <div class="social"><a href="#">
-				<i class="icon ion-social-instagram"></i></a><a href="#">
-				<i class="icon ion-social-snapchat"></i></a><a href="#">
-				<i class="icon ion-social-twitter"></i></a><a href="#">
-				<i class="icon ion-social-facebook"></i></a></div>
+                    <i class="icon ion-social-instagram"></i></a><a href="#">
+                    <i class="icon ion-social-snapchat"></i></a><a href="#">
+                    <i class="icon ion-social-twitter"></i></a><a href="#">
+                    <i class="icon ion-social-facebook"></i></a></div>
             <ul class="list-inline">
                 <li class="list-inline-item"><a href="/">Home</a></li>
                 <li class="list-inline-item"><a href="/about-the-association/">About</a></li>
                 <li class="list-inline-item"><a href="/events-information/">Events</a></li>
                 <li class="list-inline-item"><a href="/monthly-magazine-2/">Monthly Magazine</a></li>
                 <li class="list-inline-item"><a href="/monthly-magazine/">Monthly Magazine Archives</a></li>
-				<li class="list-inline-item"><a href="/photo-gallery/">Photo Gallery</a></li>
-                
+                <li class="list-inline-item"><a href="/photo-gallery/">Photo Gallery</a></li>
+
             </ul>
             <p class="copyright">TOWNSVILLE & THURINGOWA COUNTRY MUSIC ASSOCIATIONs © 2023</p>
         </footer>
@@ -49,7 +49,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
 
 
-<?php wp_footer(); ?>
+    <?php wp_footer(); ?>
 
 
 
@@ -57,5 +57,5 @@
 
 
 
-</body>
-</html>
+    </body>
+    </html>
