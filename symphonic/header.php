ff25e69d85e8c34ecb333ec4b38cc8d3dac3ee99
Jack-Walsh <jack.walsh@my.jcu.edu.au>
2023-05-20 21:05:23 +1000

    add customised buttons

--- symphonic/header.php
+++ symphonic/header.php
@@ -11,13 +11,11 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> lang="">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-<!--	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">-->
-<!--	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">-->
 
 
 	<?php wp_head(); ?>
@@ -30,12 +28,10 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'symphonic' ); ?></a>
 
 	<header id="masthead" class="site-header">
-<!--		<div class="site-top-bar">-->
-<!--			<div class="social"><a href="#">-->
-<!--					<i class="icon ion-social-facebook"></i></a></div>-->
-<!--		</div>-->
 
 		<div class="site-branding">
+
+
 			<?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
